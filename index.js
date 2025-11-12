f866ed77ce0a06009aaa95140d4fe8b128d218d3
MdMehediHasan22 <mdmehedi3799@gmail.com>
2025-11-12 07:31:14 +0600

    complette the delete and Create and Read operation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {MongoClient,ServerApiVersion} = require('mongodb');
+const {MongoClient,ServerApiVersion,ObjectId} = require('mongodb');
 const cors = require('cors');
 
 const app = express();
@@ -41,6 +41,18 @@ async function run() {
       const resultStd = await studentCollection.insertOne(newStudent);
       res.send(resultStd);
     });
+    app.delete('/users/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne (query);
+      res.send(result);
+    });
+    app.delete('/students/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await studentCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
